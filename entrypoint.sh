0914e687f5dec8d85d3fe9c3af4c940f69442269
Sandro Cavazzoni <sandro@ideaful.it>
2020-12-11 11:55:21 +0100

    Add support for publish to store

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,12 +20,18 @@ if [ -n "${INPUT_BUILDNUMBER}" ]; then
     params+=(--build-number "$INPUT_BUILDNUMBER")
 fi
 
-for group in $INPUT_GROUP; do
-    if ${isFirst} ; then
-        isFirst=false
-        appcenter distribute release --token "$INPUT_TOKEN" --app "$INPUT_APPNAME" --group $group --file "$INPUT_FILE" --release-notes "$RELEASE_NOTES" "${params[@]}"
-        releaseId=$(appcenter distribute releases list --token "$INPUT_TOKEN"  --app "$INPUT_APPNAME" | grep ID | tr -s ' ' | cut -f2 -d ' ' | sort -n -r | head -1)
-    else
-        appcenter distribute releases add-destination --token "$INPUT_TOKEN" -d $group -t group -r $releaseId --app "$INPUT_APPNAME" "${params[@]}"
-    fi
-done
+if [ "${INPUT_GROUP}" ]; then
+    for group in $INPUT_GROUP; do
+        if ${isFirst} ; then
+            isFirst=false
+            appcenter distribute release --token "$INPUT_TOKEN" --app "$INPUT_APPNAME" --group $group --file "$INPUT_FILE" --release-notes "$RELEASE_NOTES" "${params[@]}"
+            releaseId=$(appcenter distribute releases list --token "$INPUT_TOKEN"  --app "$INPUT_APPNAME" | grep ID | tr -s ' ' | cut -f2 -d ' ' | sort -n -r | head -1)
+        else
+            appcenter distribute releases add-destination --token "$INPUT_TOKEN" -d $group -t group -r $releaseId --app "$INPUT_APPNAME" "${params[@]}"
+        fi
+    done
+fi
+
+if [ "${INPUT_STORE}" ]; then
+    appcenter distribute release --token "$INPUT_TOKEN" --app "$INPUT_APPNAME" --store "$INPUT_STORE" --file "$INPUT_FILE" --release-notes "$RELEASE_NOTES" "${params[@]}"
+fi
